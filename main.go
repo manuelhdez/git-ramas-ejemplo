58f3148c98d2857c06b503da0b647a675e6fba81
Manuel Hernández <manuelhernandez@outlook.com>
2024-01-20 09:41:22 -0700

    validar si los números son mayor a 0

--- main.go
+++ main.go
@@ -8,9 +8,12 @@ func main() {
 	fmt.Println("Archivo para primer commit.")
 	fmt.Println("Segundo commit")
 
-	fmt.Printf("Mi suma es: %d\n", fn_add(10,11))
+	fmt.Printf("Mi suma es: %d\n", fn_add(10, 11))
 }
 
 func fn_add(a, b int64) int64 {
-	return a + b
+	if a > 0 && b > 0 {
+		return a + b
+	}
+	return -1
 }
