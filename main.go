04202501b8240204ce381e68f9507ae0a161ec59
Manuel Hernández <manuelhernandez@outlook.com>
2024-01-20 09:44:08 -0700

    cuarto commit de main

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func main() {
-  fmt.Println("Archivo para primer commit.")
-  fmt.Println("Segundo commit")
-  fmt.Println("Tercer commit en la rama main")
+	fmt.Println("Archivo para primer commit.")
+	fmt.Println("Segundo commit")
+	fmt.Println("Tercer commit en la rama main")
+	fmt.Println("Cuarto commit de main")
 }
