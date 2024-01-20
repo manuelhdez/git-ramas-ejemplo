9959832b43eca52349fd7f13e41d3780b88afbee
Manuel Hernández <manuelhernandez@outlook.com>
2024-01-20 09:42:46 -0700

    documentar la funcion

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	fmt.Printf("Mi suma es: %d\n", fn_add(10, 11))
 }
 
+// fn_add verifica si los numeros son mayores a 0,
+// en caso de no ser así, regresa -1
 func fn_add(a, b int64) int64 {
 	if a > 0 && b > 0 {
 		return a + b
